c030f930c18d7ab36396be4eb64e020cfce89787
chandrabs25 <22f3002048@ds.study.iitm.ac.in>
2025-05-02 18:43:10 +0530

    created new favicon

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -48,7 +48,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1746190595902,
+    __BUILD_TIMESTAMP_MS__: 1746191379e3,
     __NEXT_BASE_PATH__: ""
   });
 }
